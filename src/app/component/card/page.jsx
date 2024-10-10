7b7e022d8ac01dc993c2f4fdcb1e1ed637e381cd
Enrico Mani <manienrico13@gmail.com>
2024-10-10 17:45:47 +0300

    card coloring boundary created

--- src/app/component/card/page.jsx
+++ src/app/component/card/page.jsx
@@ -3,7 +3,7 @@ import ItemText from "../itemText/page";
 
 export default function Card({pic}){
     return(
-        <div className="flex items-center flex-col">
+        <div className="flex items-center flex-col bg-white rounded-2xl ">
             <div className="flex justify-center items-center flex-col">
                 <section>{pic}</section>
                 <ItemText item="QQQQQQQQQ" itemCalories="qqqqq" />
